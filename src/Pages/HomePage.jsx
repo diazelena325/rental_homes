86a893ab73dc03fa667e98151f7010e182fc04a6
Elena <elenadiaz32505@gmail.com>
2023-05-23 14:47:19 -0500

    update homepage

--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -1,11 +1,9 @@
 import React from 'react'
 import styled from 'styled-components'
-import Navbar from '../components/Navbar';
 import Header from '../components/Header';
 import Carousel from '../components/Carousel';
 import houseData from "../data/homes";
 import renterData from "../data/renterTypes";
-import Footer from '../components/Footer';
 import Banner from '../components/Banner';
 
 
